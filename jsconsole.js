28b846696371011a5d36760adf4ca946ac9b6129
haru <79097169+haruki1234@users.noreply.github.com>
2021-07-23 16:04:15 +0900

    Add files via upload

--- jsconsole.js
+++ jsconsole.js
@@ -1,56 +1,82 @@
-    print = {
-        log : function(text){
-            var new_element = document.createElement('div');
-            new_element.innerHTML = "> "+text.toString().replace(/\n/g,"<br>");
-            document.getElementById("view").appendChild(new_element);
-            var element = document.documentElement;
-            var bottom = element.scrollHeight - element.clientHeight;
-            window.scroll(0, bottom);
-        },
-        alert : function(text){
-            var new_element = document.createElement('div');
-            new_element.innerHTML = "> \\ "+text.toString().replace(/\n/g,"<br>")+" /";
-            new_element.style.fontSize = "22px"
-            document.getElementById("view").appendChild(new_element);
-            var element = document.documentElement;
-            var bottom = element.scrollHeight - element.clientHeight;
-            window.scroll(0, bottom);
-        },
-        warn : function(text){
-            var new_element = document.createElement('div');
-            new_element.innerHTML = "> "+text.toString().replace(/\n/g,"<br>");
-            new_element.style.color = "rgb(255,236,71)"
-            document.getElementById("view").appendChild(new_element);
-            var element = document.documentElement;
-            var bottom = element.scrollHeight - element.clientHeight;
-            window.scroll(0, bottom);
-        },
-        adv : function(text){
-            var new_element = document.createElement('div');
-            new_element.innerHTML = "! "+text.toString().replace(/\n/g,"<br>");
-            document.getElementById("view").appendChild(new_element);
-            var element = document.documentElement;
-            var bottom = element.scrollHeight - element.clientHeight;
-            window.scroll(0, bottom);
-        },
-        link : function(url){
-            var new_element = document.createElement('div');
-            new_element.innerHTML = "> <a href="+url+">"+url+"</a>";
-            document.getElementById("view").appendChild(new_element);
-            var element = document.documentElement;
-            var bottom = element.scrollHeight - element.clientHeight;
-            window.scroll(0, bottom);
-        },
-        embed : function(url){
-            var new_element = document.createElement('iframe');
-            new_element.src = url;
-            document.getElementById("view").appendChild(new_element);
-            var element = document.documentElement;
-            var bottom = element.scrollHeight - element.clientHeight;
-            window.scroll(0, bottom);
-        },
-        clear : function(){
-            document.getElementById("view").innerHTML = "";
-            start();
-        },
-    }
+print = {
+    log : function(text){
+        if (text == null){
+            return
+        }
+        var new_element = document.createElement('div');
+        new_element.innerHTML = "> "+text.replace(/\n/g,"<br>");
+        document.getElementById("view").appendChild(new_element);
+        var element = document.documentElement;
+        var bottom = element.scrollHeight - element.clientHeight;
+        window.scroll(0, bottom);
+    },
+    alert : function(text){
+        if (text == null){
+            return
+        }
+        var new_element = document.createElement('div');
+        new_element.innerHTML = "> \\ "+text.replace(/\n/g,"<br>")+" /";
+        new_element.style.fontSize = "22px";
+        document.getElementById("view").appendChild(new_element);
+        var element = document.documentElement;
+        var bottom = element.scrollHeight - element.clientHeight;
+        window.scroll(0, bottom);
+    },
+    warn : function(text){
+        if (text == null){
+            return
+        }
+        var new_element = document.createElement('div');
+        new_element.innerHTML = "> "+text.replace(/\n/g,"<br>");
+        new_element.style.color = "rgb(255,236,71)";
+        document.getElementById("view").appendChild(new_element);
+        var element = document.documentElement;
+        var bottom = element.scrollHeight - element.clientHeight;
+        window.scroll(0, bottom);
+    },
+    adv : function(text){
+        if (text == null){
+            return
+        }
+        var new_element = document.createElement('div');
+        new_element.innerHTML = "! "+text.replace(/\n/g,"<br>");
+        document.getElementById("view").appendChild(new_element);
+        var element = document.documentElement;
+        var bottom = element.scrollHeight - element.clientHeight;
+        window.scroll(0, bottom);
+    },
+    link : function(url){
+        if (url == null){
+            return
+        }
+        var new_element = document.createElement('div');
+        new_element.innerHTML = "> <a href="+url+">"+url+"</a>";
+        document.getElementById("view").appendChild(new_element);
+        var element = document.documentElement;
+        var bottom = element.scrollHeight - element.clientHeight;
+        window.scroll(0, bottom);
+    },
+    embed : function(url){
+        if (url == null){
+            return
+        }
+        var new_element = document.createElement('iframe');
+        new_element.src = url;
+        document.getElementById("view").appendChild(new_element);
+        var element = document.documentElement;
+        var bottom = element.scrollHeight - element.clientHeight;
+        window.scroll(0, bottom);
+    },
+    clear : function(){
+        document.getElementById("view").innerHTML = "";
+        start();
+    },
+}
+log = print.log;
+
+function window_s(){
+    go = function(url){location.href=url;return "go to "+url};
+    reload = function(){location.reload();return "reload now"};
+    size = function(scale){return window.parent.scale(scale)};
+    scale = function(scale){return window.parent.scale(scale)};
+}
